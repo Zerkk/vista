f8963a0e1f2715bda76673693b2e23e41388f1e5
Zerkk <32021463+Zerkk@users.noreply.github.com>
2017-09-17 00:15:38 -0500

    Create web.php

---
+++ .gitignore/web.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+ */
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource('almacen/categoria', 'CategoriaController');
+Route::patch('almacen/categoria/{id}', [
+    'as'   => 'almacen.categoria.update',
+    'uses' => 'CategoriaController@update',
+]);
+Route::resource('almacen/color', 'ColorController');
+Route::patch('almacen/color/{id}', [
+    'as'   => 'almacen.color.update',
+    'uses' => 'ColorController@update',
+]);
+Route::resource('almacen/talla', 'TallaController');
+Route::patch('almacen/talla/{id}', [
+    'as'   => 'almacen.talla.update',
+    'uses' => 'TallaController@update',
+]);
+
+Route::resource('almacen/producto', 'ProductoController');
+Route::patch('almacen/producto/{id}', [
+    'as'   => 'almacen.producto.update',
+    'uses' => 'ProductoController@update',
+]);
+Route::post('ururl/crear', 'ProductoController@store');
